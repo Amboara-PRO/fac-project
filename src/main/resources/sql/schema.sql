4bb37e82de0763234895b8a039019e7a7260a8a1
Amboara <andriamalalaamboarafaneva@gmail.com>
2026-04-22 11:51:49 +0300

    feat(database): update database structure

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,4 +1,9 @@
 CREATE TYPE gender AS ENUM ('MALE', 'FEMALE');
+CREATE TYPE frequency AS ENUM ('WEEKLY', 'MONTHLY', 'ANNUALLY', 'PUNCTUALLY');
+CREATE TYPE activity_status AS ENUM ('ACTIVE', 'INACTIVE');
+CREATE TYPE payment_mode AS ENUM ('CASH', 'MOBILE_BANKING', 'BANK_TRANSFER');
+CREATE TYPE mobile_service AS ENUM ('AIRTEL_MONEY', 'MVOLA', 'ORANGE_MONEY');
+CREATE TYPE bank_name AS ENUM ('BRED', 'MCB', 'BMOI', 'BOA', 'BGFI', 'AFG', 'ACCES_BAQUE', 'BAOBAB', 'SIPEM');
 CREATE TYPE member_occupation AS ENUM (
     'JUNIOR',
     'SENIOR',
@@ -72,4 +77,51 @@ CREATE TABLE collectivity_structure (
                                         FOREIGN KEY (vice_president_id) REFERENCES members(id),
                                         FOREIGN KEY (treasurer_id) REFERENCES members(id),
                                         FOREIGN KEY (secretary_id) REFERENCES members(id)
+);
+CREATE TABLE membership_fees (
+                                 id VARCHAR(9) PRIMARY KEY,
+                                 collectivity_id VARCHAR(9) NOT NULL,
+                                 eligible_from DATE NOT NULL,
+                                 frequency frequency NOT NULL,
+                                 amount DECIMAL(15, 2) NOT NULL CHECK (amount >= 0),
+                                 label VARCHAR(255),
+                                 status activity_status DEFAULT 'ACTIVE',
+                                 CONSTRAINT fk_fee_collectivity FOREIGN KEY (collectivity_id) REFERENCES collectivities(id) ON DELETE CASCADE
+);
+CREATE TABLE financial_accounts (
+                                    id VARCHAR(9) PRIMARY KEY,
+                                    collectivity_id VARCHAR(9) NOT NULL,
+                                    account_type payment_mode NOT NULL,
+                                    balance DECIMAL(15, 2) DEFAULT 0,
+                                    CONSTRAINT fk_account_collectivity FOREIGN KEY (collectivity_id) REFERENCES collectivities(id) ON DELETE CASCADE
+);
+CREATE TABLE mobile_banking_accounts (
+                                         account_id VARCHAR(9) PRIMARY KEY REFERENCES financial_accounts(id) ON DELETE CASCADE,
+                                         holder_name VARCHAR(150) NOT NULL,
+                                         service mobile_service NOT NULL,
+                                         mobile_number BIGINT NOT NULL
+);
+
+CREATE TABLE bank_accounts (
+                               account_id VARCHAR(9) PRIMARY KEY REFERENCES financial_accounts(id) ON DELETE CASCADE,
+                               holder_name VARCHAR(150) NOT NULL,
+                               bank_name bank_name NOT NULL,
+                               bank_code INT,
+                               branch_code INT,
+                               account_number BIGINT,
+                               account_key INT
+);
+CREATE TABLE transactions (
+                              id VARCHAR(9) PRIMARY KEY,
+                              creation_date DATE DEFAULT CURRENT_DATE,
+                              amount DECIMAL(15, 2) NOT NULL,
+                              payment_mode payment_mode NOT NULL,
+
+                              member_id VARCHAR(9) NOT NULL,
+                              membership_fee_id VARCHAR(9),
+                              account_id VARCHAR(9) NOT NULL,
+
+                              CONSTRAINT fk_trans_member FOREIGN KEY (member_id) REFERENCES members(id),
+                              CONSTRAINT fk_trans_fee FOREIGN KEY (membership_fee_id) REFERENCES membership_fees(id),
+                              CONSTRAINT fk_trans_account FOREIGN KEY (account_id) REFERENCES financial_accounts(id)
 );
