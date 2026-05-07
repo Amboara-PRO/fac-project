ca1a8d9d359220a59ceeb47e32c43a32621717f7
Amboara <andriamalalaamboarafaneva@gmail.com>
2026-05-07 13:13:19 +0300

    feat(security) : Add security for each API

--- src/main/java/com/example/FacProject/FacProjectApplication.java
+++ src/main/java/com/example/FacProject/FacProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.FacProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class FacProjectApplication {
 
 	public static void main(String[] args) {
